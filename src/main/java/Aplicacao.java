44bfac5e1e5757deecc0db816dc39a9f594a3479
Lucas Zanon <lucaspzanonn@outlook.com>
2022-03-31 23:17:42 -0300

    Alteração na fábrica de conexões ;-/

--- src/main/java/Aplicacao.java
+++ src/main/java/Aplicacao.java
@@ -6,11 +6,11 @@ public class Aplicacao {
 
     public static void main(String[] args){
         Endpoint.publish(
-                "http://localhost:8080/analise-servidor",
+                "http://localhost:8080/analise-servidor?wsdl",
                 new AnaliseServico()
         );
         Endpoint.publish(
-                "http://localhost:8080/compra-servidor",
+                "http://localhost:8080/compra-servidor?wsdl",
                 new CompraServico()
         );
 
